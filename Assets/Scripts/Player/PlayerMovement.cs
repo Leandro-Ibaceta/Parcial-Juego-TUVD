ddfb4a8186a764421b03188abe1598885c54305c
Leandro Ibaceta <72288170+Leandro-Ibaceta@users.noreply.github.com>
2025-06-02 22:34:51 -0300

    Movement System en progreso

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,16 +10,29 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _input;
     private Vector3 _movementRelativeToCamera;
 
+    //Animation variables
+    [SerializeField] private Animator _animator;
+    private int _isWalkingHash, _isRunningHash;
+   
+
     private void Awake()
     {
         _playerRb = GetComponent<Rigidbody>();
         _playerInput = GetComponent<PlayerInput>();
     }
 
+    private void Start()
+    {
+        _isWalkingHash = Animator.StringToHash("IsWalking");
+        _isRunningHash = Animator.StringToHash("IsRunning");
+    }
+
+
     private void Update()
     {
         _input = _playerInput.actions["Move"].ReadValue<Vector2>();
         _movementRelativeToCamera = MoveRelativeToCamera(_input);
+        RotatePlayer();
     }
 
     private void FixedUpdate()
@@ -35,7 +48,36 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer(Vector3 input)
     {
+        bool isWalking = _animator.GetBool(_isWalkingHash);
+        bool isRunning = _animator.GetBool(_isRunningHash);
+        bool movePressed = _input.x != 0 || _input.y != 0;
+
+        if (movePressed && !isWalking)
+            _animator.SetBool(_isWalkingHash, true);
+
+        if ((!movePressed && isWalking))
+            _animator.SetBool(_isWalkingHash, false);
+
+        
+        
+
         _playerRb.MovePosition(transform.position + input * _speed * Time.deltaTime);
+       
+    }
+
+    private void RotatePlayer()
+    {
+        float rotationSpeed = 5;
+        Vector3 posToLookAt;
+        posToLookAt.x = _movementRelativeToCamera.x;
+        posToLookAt.y = 0;
+        posToLookAt.z = _movementRelativeToCamera.z;
+        posToLookAt = posToLookAt.normalized;
+
+        Quaternion rotation = transform.rotation;
+        Quaternion targetRotation = posToLookAt == Vector3.zero ? Quaternion.identity : Quaternion.LookRotation(posToLookAt);
+        
+        transform.rotation = Quaternion.Slerp(rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     private Vector3 MoveRelativeToCamera(Vector2 input)
@@ -45,5 +87,19 @@ public class PlayerMovement : MonoBehaviour
         camF.y = camR.y = 0;
         camF.Normalize(); camR.Normalize();
         return input.y * camF + input.x * camR;
+    }
+
+    public void Crouch(InputAction.CallbackContext callbackContext)
+    {
+        if(callbackContext.performed)
+        {
+            _animator.SetBool("IsCrouching", true);
+        }
+        if (callbackContext.canceled)
+        {
+            _animator.SetBool("IsCrouching", false);
+
+        }
+        
     }
 }
