8ac99888d8e361c605268c680b0c1e5dfde0f558
Leandro Ibaceta <72288170+Leandro-Ibaceta@users.noreply.github.com>
2025-06-03 23:24:43 -0300

    Li movement system (#4)
    * Movimiento personaje con algunos bugs en la animacion

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,22 +4,57 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 10f;
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _WalkingSpeed = 10f;
+    [SerializeField] private float _CrouchingSpeed = 10f;
+    private float _speed;
     private Rigidbody _playerRb;
     private PlayerInput _playerInput;
+    private CapsuleCollider _playerCollider;
     private Vector2 _input;
     private Vector3 _movementRelativeToCamera;
+
+<<<<<<< HEAD
+    private bool _onStealth = false;
+    
+    //Animation variables
+    [SerializeField] private Animator _animator;
+
+    private int _isWalkingHash, _isRunningHash;
+
+   public bool OnStealth 
+    {
+        get { return _onStealth; }
+    }
+=======
+    //Animation variables
+    [SerializeField] private Animator _animator;
+    private int _isWalkingHash, _isRunningHash;
+>>>>>>> f15cf7f468fb1059aac6ddf436790db624d6f959
+   
 
     private void Awake()
     {
         _playerRb = GetComponent<Rigidbody>();
         _playerInput = GetComponent<PlayerInput>();
+        _playerCollider = GetComponent<CapsuleCollider>();
     }
+
+    private void Start()
+    {
+<<<<<<< HEAD
+        _speed = _WalkingSpeed;
+=======
+>>>>>>> f15cf7f468fb1059aac6ddf436790db624d6f959
+        _isWalkingHash = Animator.StringToHash("IsWalking");
+        _isRunningHash = Animator.StringToHash("IsRunning");
+    }
+
 
     private void Update()
     {
         _input = _playerInput.actions["Move"].ReadValue<Vector2>();
         _movementRelativeToCamera = MoveRelativeToCamera(_input);
+        RotatePlayer();
     }
 
     private void FixedUpdate()
@@ -35,7 +70,47 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer(Vector3 input)
     {
+        bool isWalking = _animator.GetBool(_isWalkingHash);
+        bool isRunning = _animator.GetBool(_isRunningHash);
+        bool movePressed = _input.x != 0 || _input.y != 0;
+
+        if (movePressed && !isWalking)
+            _animator.SetBool(_isWalkingHash, true);
+
+        if ((!movePressed && isWalking))
+            _animator.SetBool(_isWalkingHash, false);
+<<<<<<< HEAD
+           
+=======
+>>>>>>> f15cf7f468fb1059aac6ddf436790db624d6f959
+
+        
+        
+
         _playerRb.MovePosition(transform.position + input * _speed * Time.deltaTime);
+       
+    }
+
+    private void RotatePlayer()
+    {
+        float rotationSpeed = 5;
+        Vector3 posToLookAt;
+        posToLookAt.x = _movementRelativeToCamera.x;
+        posToLookAt.y = 0;
+        posToLookAt.z = _movementRelativeToCamera.z;
+        posToLookAt = posToLookAt.normalized;
+
+<<<<<<< HEAD
+        
+        Quaternion targetRotation = posToLookAt == Vector3.zero ? transform.rotation : Quaternion.LookRotation(posToLookAt);
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+=======
+        Quaternion rotation = transform.rotation;
+        Quaternion targetRotation = posToLookAt == Vector3.zero ? Quaternion.identity : Quaternion.LookRotation(posToLookAt);
+        
+        transform.rotation = Quaternion.Slerp(rotation, targetRotation, rotationSpeed * Time.deltaTime);
+>>>>>>> f15cf7f468fb1059aac6ddf436790db624d6f959
     }
 
     private Vector3 MoveRelativeToCamera(Vector2 input)
@@ -45,5 +120,40 @@ public class PlayerMovement : MonoBehaviour
         camF.y = camR.y = 0;
         camF.Normalize(); camR.Normalize();
         return input.y * camF + input.x * camR;
+    }
+
+    public void Crouch(InputAction.CallbackContext callbackContext)
+    {
+<<<<<<< HEAD
+        float standingHeight = 2f;
+        float crouchingHeight = 0.75f;
+
+        if(callbackContext.performed)
+        {
+            _playerCollider.height = crouchingHeight;
+            _playerCollider.center = new Vector3(0, -0.5f, 0);
+            _speed = _CrouchingSpeed;
+            _animator.SetBool("IsCrouching", true);
+            _onStealth = true;
+        }
+        if (callbackContext.canceled)
+        {
+            _playerCollider.height = standingHeight;
+            _playerCollider.center = Vector3.zero;
+            _speed = _WalkingSpeed;
+            _animator.SetBool("IsCrouching", false);
+            _onStealth = false;
+=======
+        if(callbackContext.performed)
+        {
+            _animator.SetBool("IsCrouching", true);
+        }
+        if (callbackContext.canceled)
+        {
+            _animator.SetBool("IsCrouching", false);
+>>>>>>> f15cf7f468fb1059aac6ddf436790db624d6f959
+
+        }
+        
     }
 }
